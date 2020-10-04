86201f4bc3fe05a59e972bc5dde861c1e9537bcb
gabs086 <agoncillogabriel08@gmail.com>
2020-10-03 22:47:40 -0700

    Using state in react-native

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import { Text, View } from "react-native";
+import React, { useState } from 'react';
+import { Text, View, Button } from 'react-native';
 import { styles } from './styles/style';
 
 export default function App(props) {
-  console.log(props);
+  const [name, setName] = useState('gab');
+  const [person, setPerson] = useState({ name: 'mario', age: 40 });
+
+  const clickHandler = () => {
+    setName('angge');
+    setPerson({ name: 'gavee', age: 45 });
+  };
+
   return (
     <View style={styles.container}>
-      
-    <View style={styles.header}>
-      <Text style={styles.boldText}>Hello, World!</Text>
-    </View>
+      <Text>My name is {name}</Text>
 
-    <View style={styles.body}>
-      <Text>Lorem ipsum <Text style={styles.boldText}>dolor</Text> sit amet.</Text>
-      <Text>Lorem ipsum dolor sit amet.</Text>
-      <Text>Lorem ipsum dolor sit amet.</Text>
+      <Text>His name is {person.name} and his age is {person.age}</Text>
+
+      <View style={styles.buttonContainer}>
+        <Button title='update state' onPress={clickHandler} />  
+      </View>
+
     </View>
-    
-  </View>
   );
 }
--- styles/style.js
+++ styles/style.js
@@ -2,20 +2,12 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    header: {
-      backgroundColor: 'pink',
-      padding: 20,
-    },
-    body: {
-      backgroundColor: 'yellow',
-      padding: 20,
-    },
-    boldText: {
-      fontWeight: 'bold',
-    }
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      buttonContainer: {
+        marginTop: 20,
+      }
   });
