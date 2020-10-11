468e7751f6c33b41f32ae68833bc9409afd3c4c3
gabs086 <agoncillogabriel08@gmail.com>
2020-10-10 20:32:47 -0700

    Start of the Gamezone Review app of the net ninja

---
+++ screens/ReviewDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styles } from '../styles/reviewDetails';
+
+export default function ReviewDetails() {
+  return (
+    <View style={styles.container}>
+      <Text>ReviewDetails Screen</Text>
+    </View>
+  );
+}
