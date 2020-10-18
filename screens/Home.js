0f0d5831cba513701b9dac89d6b016ccf6fd276e
gabs086 <agoncillogabriel08@gmail.com>
2020-10-18 13:52:43 -0700

    Passing data between screens

--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { styles } from '../styles/globalStyles';
 
 export default function Home(props) {
 
-    const navigateToDetails = _ => {
-      // props.navigation.navigate('ReviewDetails');
-      //Other way of going to another screen
-      props.navigation.push('ReviewDetails')
-    }
+  const [reviews, setReviews] = useState([
+    { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+    { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
+    { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
+  ]);
+
+  // Better way to render components. My Style 
+  const renderItem = ({ item }) => {
+    return (
+      // When navigating and passing data, the 2nd paramete must be an object 
+        <TouchableOpacity onPress={_ => props.navigation.navigate('ReviewDetails', item)}>
+            <Text style={styles.titleText}>{item.title}</Text>
+        </TouchableOpacity>
+    );
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.titleText}>Home Screen </Text>
-      <Button title="Review Page" onPress={navigateToDetails}/>
+      {/* <Text style={styles.titleText}>Home Screen </Text> */}
+          <FlatList 
+            data={reviews}
+            renderItem={renderItem}
+          />
     </View>
   );
 }
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -3,15 +3,13 @@ import { View, Text, Button } from 'react-native';
 import { styles } from '../styles/globalStyles';
 
 export default function ReviewDetails(props) {
-
-  const backToHome = _ => {
-    props.navigation.goBack();
-  }
-
+  console.log(props);
   return (
     <View style={styles.container}>
-      <Text>ReviewDetails Screen</Text>
-      <Button title="Back to home screen" onPress={backToHome} />
+      {/* Use a props.route.params.title to get a specific data in the parameter in the main screen  */}
+      <Text>{props.route.params.title}</Text>
+      <Text>{props.route.params.body}</Text>
+      <Text>{props.route.params.rating}</Text>
     </View>
   );
 }
